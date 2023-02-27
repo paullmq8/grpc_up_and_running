d202789335b0a41c55c5f391bc8fd7f59554c641
paullmq8 <paullmq8@gmail.com>
2023-02-27 13:57:05 -0500

    finish learning chapter 3

--- ecommerce/client/main.go
+++ ecommerce/client/main.go
@@ -16,132 +16,132 @@ import (
 )
 
 const (
-    productAddress = "localhost:50051"
-    orderAddress = "localhost:50052"
+	productAddress = "localhost:50051"
+	orderAddress   = "localhost:50052"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-    // call two unary RPC methods
-    // wg.Add(1)
-    // go callAddProductAndGetProduct()
-    // wg.Add(1)
-    // go callGetOrder()
-    // call server-side streaming RPC method
-    // wg.Add(1)
-    // go callSearchOrders()
-    // wg.Add(1)
-    // go callUpdateOrders()
-    wg.Add(1)
-    go callProcessOrders()
-    wg.Wait()
+	// call two unary RPC methods
+	// wg.Add(1)
+	// go callAddProductAndGetProduct()
+	// wg.Add(1)
+	// go callGetOrder()
+	// call server-side streaming RPC method
+	// wg.Add(1)
+	// go callSearchOrders()
+	// wg.Add(1)
+	// go callUpdateOrders()
+	wg.Add(1)
+	go callProcessOrders()
+	wg.Wait()
 }
 
-// callAddProductAndGetProduct calls the AddProduct and GetProduct gRPC methods of the ProductInfoServer 
+// callAddProductAndGetProduct calls the AddProduct and GetProduct gRPC methods of the ProductInfoServer
 // in unary RPC mode.
 func callAddProductAndGetProduct() {
-    defer wg.Done()
-    conn, err := grpc.Dial(productAddress, grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewProductInfoClient(conn)
-    name := "Apple iPhone 11"
-    description := `Meet Apple iPhone 11. All-new dual-camera system with
+	defer wg.Done()
+	conn, err := grpc.Dial(productAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewProductInfoClient(conn)
+	name := "Apple iPhone 11"
+	description := `Meet Apple iPhone 11. All-new dual-camera system with
               Ultra Wide and Night mode.`
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description})
-    if err != nil {
-        log.Fatalf("Could not add product: %v", err)
-    }
-    log.Printf("AddProduct: Product ID: %s added successfully", r.Value)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.AddProduct(ctx, &pb.Product{Name: name, Description: description})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("AddProduct: Product ID: %s added successfully", r.Value)
 
-    product, err := c.GetProduct(ctx, &pb.ProductID{Value: r.Value})
-    if err != nil {
-        log.Fatalf("Could not get product: %v", err)
-    }
-    log.Println("GetProduct: Product: ", product.String())
+	product, err := c.GetProduct(ctx, &pb.ProductID{Value: r.Value})
+	if err != nil {
+		log.Fatalf("Could not get product: %v", err)
+	}
+	log.Println("GetProduct: Product: ", product.String())
 }
 
-// callGetOrder calls the GetOrder gRPC methods of the OrderManagementServer 
+// callGetOrder calls the GetOrder gRPC methods of the OrderManagementServer
 // in unary RPC mode.
 func callGetOrder() {
-    defer wg.Done()
-    // Setting up a connection to the server.
-    conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewOrderManagementClient(conn)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
+	defer wg.Done()
+	// Setting up a connection to the server.
+	conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-    num := strconv.Itoa(rand.Intn(5) + 102) // 102 - 106
-    retrievedOrder, err := c.GetOrder(ctx, &wrappers.StringValue{Value: num})
-    
-    if err != nil {
-        log.Fatalf("can not get order: %v", err)
-    }
-    if retrievedOrder != nil {
-        log.Println("GetOrder: Response -> : ", retrievedOrder)
-    } else {
-        log.Println("GetOrder: Response -> : No order found for id ", num)
-    }
+	num := strconv.Itoa(rand.Intn(5) + 102) // 102 - 106
+	retrievedOrder, err := c.GetOrder(ctx, &wrappers.StringValue{Value: num})
+
+	if err != nil {
+		log.Fatalf("can not get order: %v", err)
+	}
+	if retrievedOrder != nil {
+		log.Println("GetOrder: Response -> : ", retrievedOrder)
+	} else {
+		log.Println("GetOrder: Response -> : No order found for id ", num)
+	}
 }
 
-// callSearchOrders calls the SearchOrders gRPC methods of the OrderManagementServer 
+// callSearchOrders calls the SearchOrders gRPC methods of the OrderManagementServer
 // in server streaming RPC mode.
 func callSearchOrders() {
-    defer wg.Done()
-    // Setting up a connection to the server.
-    conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewOrderManagementClient(conn)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    searchStream, _ := c.SearchOrders(ctx, &wrappers.StringValue{Value: "Google"})
-    for {
-        searchOrder, err := searchStream.Recv()
-        if err == io.EOF {
-            break
-        }
-        if err == nil {
-            log.Print("Search Result : ", searchOrder)
-        }
-    }
+	defer wg.Done()
+	// Setting up a connection to the server.
+	conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Hour)
+	defer cancel()
+	searchStream, _ := c.SearchOrders(ctx, &wrappers.StringValue{Value: "Google"})
+	for {
+		searchOrder, err := searchStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err == nil {
+			log.Print("Search Result : ", searchOrder)
+		}
+	}
 }
 
 // callUpdateOrders calls the UpdateOrders gRPC methods of the OrderManagementServer
 // in client streaming RPC mode.
 func callUpdateOrders() {
-    defer wg.Done()
-    // Setting up a connection to the server.
-    conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewOrderManagementClient(conn)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    updateStream, err := c.UpdateOrders(ctx)
-    if err != nil {
-        log.Fatalf("%v.UpdateOrders(_) = _, %v", c, err)
-    }
+	defer wg.Done()
+	// Setting up a connection to the server.
+	conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	updateStream, err := c.UpdateOrders(ctx)
+	if err != nil {
+		log.Fatalf("%v.UpdateOrders(_) = _, %v", c, err)
+	}
 
-    // Update Orders : Client streaming scenario
-	updOrder1 := &pb.Order{Id: "102", Items:[]string{"Google Pixel 3A", "Google Pixel Book"}, Destination:"Mountain View, CA", Price:1100.00}
-	updOrder2 := &pb.Order{Id: "103", Items:[]string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination:"San Jose, CA", Price:2800.00}
-	updOrder3 := &pb.Order{Id: "104", Items:[]string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination:"Mountain View, CA", Price:2200.00}
+	// Update Orders : Client streaming scenario
+	updOrder1 := &pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Google Pixel Book"}, Destination: "Mountain View, CA", Price: 1100.00}
+	updOrder2 := &pb.Order{Id: "103", Items: []string{"Apple Watch S4", "Mac Book Pro", "iPad Pro"}, Destination: "San Jose, CA", Price: 2800.00}
+	updOrder3 := &pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub", "iPad Mini"}, Destination: "Mountain View, CA", Price: 2200.00}
 
-    // // Updating order 1
+	// // Updating order 1
 	if err := updateStream.Send(updOrder1); err != nil {
 		log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder1, err)
 	}
@@ -156,41 +156,41 @@ func callUpdateOrders() {
 		log.Fatalf("%v.Send(%v) = %v", updateStream, updOrder3, err)
 	}
 
-    updateRes, err := updateStream.CloseAndRecv()
-    if err != nil {
-        log.Fatalf("%v.CloseAndRecv() got error %v, want %v", updateStream, err, nil)
-    }
+	updateRes, err := updateStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", updateStream, err, nil)
+	}
 
-    log.Printf("Update Orders Res : %s", updateRes)
+	log.Printf("Update Orders Res : %s", updateRes)
 }
 
 // callProcessOrders calls the ProcessOrders gRPC methods of the OrderManagementServer
 // in bidirectional streaming RPC mode.
 func callProcessOrders() {
-    defer wg.Done()
-    // Setting up a connection to the server.
-    conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewOrderManagementClient(conn)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    streamProcOrder, err := c.ProcessOrders(ctx)
+	defer wg.Done()
+	// Setting up a connection to the server.
+	conn, err := grpc.Dial(orderAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	streamProcOrder, err := c.ProcessOrders(ctx)
 	if err != nil {
 		log.Fatalf("%v.ProcessOrders(_) = _, %v", c, err)
 	}
 
-	if err := streamProcOrder.Send(&wrappers.StringValue{Value:"102"}); err != nil {
+	if err := streamProcOrder.Send(&wrappers.StringValue{Value: "102"}); err != nil {
 		log.Fatalf("%v.Send(%v) = %v", c, "102", err)
 	}
 
-	if err := streamProcOrder.Send(&wrappers.StringValue{Value:"103"}); err != nil {
+	if err := streamProcOrder.Send(&wrappers.StringValue{Value: "103"}); err != nil {
 		log.Fatalf("%v.Send(%v) = %v", c, "103", err)
 	}
 
-	if err := streamProcOrder.Send(&wrappers.StringValue{Value:"104"}); err != nil {
+	if err := streamProcOrder.Send(&wrappers.StringValue{Value: "104"}); err != nil {
 		log.Fatalf("%v.Send(%v) = %v", c, "104", err)
 	}
 
@@ -198,7 +198,7 @@ func callProcessOrders() {
 	go asncClientBidirectionalRPC(streamProcOrder, channel)
 	time.Sleep(time.Millisecond * 1000)
 
-	if err := streamProcOrder.Send(&wrappers.StringValue{Value:"101"}); err != nil {
+	if err := streamProcOrder.Send(&wrappers.StringValue{Value: "101"}); err != nil {
 		log.Fatalf("%v.Send(%v) = %v", c, "101", err)
 	}
 	if err := streamProcOrder.CloseSend(); err != nil {
@@ -213,9 +213,9 @@ func asncClientBidirectionalRPC(streamProcOrder pb.OrderManagement_ProcessOrders
 		if errProcOrder == io.EOF {
 			break
 		}
-        if combinedShipment != nil {
-		    log.Println("Combined shipment : ", combinedShipment.OrdersList)
-        }
+		if combinedShipment != nil {
+			log.Println("Combined shipment : ", combinedShipment.OrdersList)
+		}
 	}
 	close(c)
 }
--- ecommerce/server/order_management_server.go
+++ ecommerce/server/order_management_server.go
@@ -13,79 +13,79 @@ import (
 )
 
 const (
-    orderBatchSize = 3
+	orderBatchSize = 3
 )
 
 type orderManagementServer struct {
-    orderMap map[string]*pb.Order
-    pb.UnimplementedOrderManagementServer
+	orderMap map[string]*pb.Order
+	pb.UnimplementedOrderManagementServer
 }
 
 // PrepareOrders is a utility method and prepares the orderMap with some sample orders
 func (s *orderManagementServer) PrepareOrders() {
-    if s.orderMap == nil {
-        s.orderMap = make(map[string]*pb.Order)
-    }
-    s.orderMap["102"] = &pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
+	if s.orderMap == nil {
+		s.orderMap = make(map[string]*pb.Order)
+	}
+	s.orderMap["102"] = &pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
 	s.orderMap["103"] = &pb.Order{Id: "103", Items: []string{"Apple Watch S4"}, Destination: "San Jose, CA", Price: 400.00}
 	s.orderMap["104"] = &pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
 	s.orderMap["105"] = &pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
 	s.orderMap["106"] = &pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
-    log.Println("orderMap is prepared!")
+	log.Println("orderMap is prepared!")
 }
 
 // GetOrder returns the order for the given order id in unary RPC mode
 func (s *orderManagementServer) GetOrder(ctx context.Context, orderId *wrappers.StringValue) (*pb.Order, error) {
-    ord, ok := s.orderMap[orderId.Value]
-    if !ok {
-        log.Println("GetOrder is done but no order found.")
-    } else {
-        log.Printf("GetOrder is done. OrderId: %v", orderId.Value)
-    }
-    return ord, nil
+	ord, ok := s.orderMap[orderId.Value]
+	if !ok {
+		log.Println("GetOrder is done but no order found.")
+	} else {
+		log.Printf("GetOrder is done. OrderId: %v", orderId.Value)
+	}
+	return ord, nil
 }
 
 // SearchOrders returns the orders for the given search query in server streaming RPC mode
 func (s *orderManagementServer) SearchOrders(searchQuery *wrappers.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
-    for key, order := range s.orderMap {
-        log.Print(key, order)
-        for _, itemStr := range order.Items {
-            log.Print(itemStr)
-            if strings.Contains(itemStr, searchQuery.Value) {
-                // Send the matching orders in a stream 
-                err := stream.Send(order)
-                if err != nil {
-                    return fmt.Errorf("error sending message to stream: %v", err)
-                }
-                log.Print("Matching Order Found : " + key)
-                break
-            }
-        }
-    }
-    return nil
+	for key, order := range s.orderMap {
+		log.Print(key, order)
+		for _, itemStr := range order.Items {
+			log.Print(itemStr)
+			if strings.Contains(itemStr, searchQuery.Value) {
+				// Send the matching orders in a stream
+				err := stream.Send(order)
+				if err != nil {
+					return fmt.Errorf("error sending message to stream: %v", err)
+				}
+				log.Print("Matching Order Found : " + key)
+				break
+			}
+		}
+	}
+	return nil
 }
 
 // UpdateOrders updates the orders in client streaming RPC mode
 func (s *orderManagementServer) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) error {
-    ordersStr := "Updated Order IDs :"
-    for {
-        order, err := stream.Recv()
-        if err == io.EOF {
-            // Finished reading the order stream.
-            return stream.SendAndClose(
-                &wrappers.StringValue{Value: "Orders processed " + strings.TrimSuffix(ordersStr, ", ")})
-        }
-        // Update order
-        s.orderMap[order.Id] = order
+	ordersStr := "Updated Order IDs :"
+	for {
+		order, err := stream.Recv()
+		if err == io.EOF {
+			// Finished reading the order stream.
+			return stream.SendAndClose(
+				&wrappers.StringValue{Value: "Orders processed " + strings.TrimSuffix(ordersStr, ", ")})
+		}
+		// Update order
+		s.orderMap[order.Id] = order
 
-        log.Println("Order ID ", order.Id, ": Updated")
-        ordersStr += order.Id + ", "
-    }
+		log.Println("Order ID ", order.Id, ": Updated")
+		ordersStr += order.Id + ", "
+	}
 }
 
 func (s *orderManagementServer) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
-    batchMarker := 1
-	combinedShipmentMap := make(map[string]pb.CombinedShipment)
+	batchMarker := 1
+	combinedShipmentMap := make(map[string]*pb.CombinedShipment)
 	for {
 		orderId, err := stream.Recv()
 		log.Printf("Reading Proc order : %s", orderId)
@@ -94,7 +94,7 @@ func (s *orderManagementServer) ProcessOrders(stream pb.OrderManagement_ProcessO
 			// Send remaining shipments
 			log.Printf("EOF : %s", orderId)
 			for _, shipment := range combinedShipmentMap {
-				if err := stream.Send(&shipment); err != nil {
+				if err := stream.Send(shipment); err != nil {
 					return err
 				}
 			}
@@ -105,8 +105,12 @@ func (s *orderManagementServer) ProcessOrders(stream pb.OrderManagement_ProcessO
 			return err
 		}
 
-        orderIdValue := orderId.GetValue()
-		destination := s.orderMap[orderIdValue].Destination
+		orderIdValue := orderId.GetValue()
+		existingOrder, ok := s.orderMap[orderIdValue]
+		if !ok {
+			continue
+		}
+		destination := existingOrder.Destination
 		shipment, found := combinedShipmentMap[destination]
 
 		if found {
@@ -114,22 +118,26 @@ func (s *orderManagementServer) ProcessOrders(stream pb.OrderManagement_ProcessO
 			shipment.OrdersList = append(shipment.OrdersList, ord)
 			combinedShipmentMap[destination] = shipment
 		} else {
-			comShip := pb.CombinedShipment{Id: "cmb - " + (s.orderMap[orderIdValue].Destination), Status: "Processed!", }
+			comShip := &pb.CombinedShipment{
+				Id:         "cmb - " + (s.orderMap[orderIdValue].Destination),
+				Status:     "Processed!",
+				OrdersList: make([]*pb.Order, 3),
+			}
 			ord := s.orderMap[orderIdValue]
-			comShip.OrdersList = append(shipment.OrdersList, ord)
+			comShip.OrdersList = append(comShip.OrdersList, ord)
 			combinedShipmentMap[destination] = comShip
 			log.Print(len(comShip.OrdersList), comShip.GetId())
 		}
 
 		if batchMarker == orderBatchSize {
 			for _, comb := range combinedShipmentMap {
-				log.Printf("Shipping : %v -> %v" , comb.Id, len(comb.OrdersList))
-				if err := stream.Send(&comb); err != nil {
+				log.Printf("Shipping : %v -> %v", comb.Id, len(comb.OrdersList))
+				if err := stream.Send(comb); err != nil {
 					return err
 				}
 			}
 			batchMarker = 0
-			combinedShipmentMap = make(map[string]pb.CombinedShipment)
+			combinedShipmentMap = make(map[string]*pb.CombinedShipment)
 		} else {
 			batchMarker++
 		}
